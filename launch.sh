1d49266365bb6bd74def3f12c33e7ae8e000b0cb
Ivan Fateev <ivan.fateev@microsoft.com>
2018-01-22 15:02:54 +0300

    redirecting all output to file

--- launch.sh
+++ launch.sh
@@ -2,6 +2,6 @@
 sudo apt-get update && sudo apt-get -y install python-pip
 python -m pip install mcstatus
 java -Xmx512M -Xms512M -jar minecraft_server.1.12.2.jar nogui&
-/usr/bin/python reportGameRooms.py& >> report.log
+/usr/bin/python reportGameRooms.py& 2>> report.log
 wait
 echo "Server shutting down..."

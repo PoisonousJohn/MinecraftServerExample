e74881fa87f38c7d45a26f5a3f7fa1a312641d59
Ivan Fateev <ivan.fateev@microsoft.com>
2018-01-22 16:28:06 +0300

    make sure /etc/profile variables are available for python script

--- launch.sh
+++ launch.sh
@@ -2,6 +2,7 @@
 sudo apt-get update && sudo apt-get -y install python-pip
 python -m pip install mcstatus
 java -Xmx512M -Xms512M -jar minecraft_server.1.12.2.jar nogui&
+source /etc/profile
 /usr/bin/python reportGameRooms.py& 2>> report.log
 wait
 echo "Server shutting down..."

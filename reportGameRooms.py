70971d0e4f1c44fd331472342a8545d6fba51a10
Ivan Fateev <ivan.fateev@microsoft.com>
2018-01-22 14:32:37 +0300

    added http callback invoking

--- reportGameRooms.py
+++ reportGameRooms.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
 import time
+import json
+import os
+import urllib2
+import socket
 from time import sleep
 from mcstatus import MinecraftServer
 
 
 def main():
+
+    hostname = socket.gethostname()
+    report_url = os.environ['REPORT_ROOMS_URL']
+    if not report_url:
+        raise Exception('REPORT_ROOMS_URL env var should be set')
 
     # retry until started
     while 1:
@@ -19,6 +28,18 @@ def main():
                     status.players.online,
                     status.latency,
                     time.strftime("%d.%m.%Y %H:%M:%S")))
+                req = urllib2.request(report_url)
+                data = {
+                    'nodes' : [
+                        {
+                            'nodeId': hostname,
+                            'rooms': status.players.online
+                        }
+                    ]
+                }
+                req.add_header('Content-Type', 'application/json')
+                response = urllib2.urlopen(req, json.dumps(data))
+                print('Reported server rooms: {0}'.format(response.info()))
                 sleep(5)
         except:
             print("Failed to connect to server. Retrying")

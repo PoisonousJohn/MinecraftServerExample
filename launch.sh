aa26488e50980693c98bcd8c8f8ee800a4296f1b
Ivan Fateev <ivan.fateev@microsoft.com>
2018-01-22 13:14:31 +0300

    added game rooms reporter

--- launch.sh
+++ launch.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-java -Xmx512M -Xms512M -jar minecraft_server.1.12.2.jar nogui
+sudo apt-get update && sudo apt-get -y install python-pip
+python -m pip install mcstatus
+/usr/bin/python reportGameRooms.py& >> report.log
+java -Xmx512M -Xms512M -jar minecraft_server.1.12.2.jar nogui&
+wait
+echo "Server shutting down..."
---
+++ reportGameRooms.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import time
+from time import sleep
+from mcstatus import MinecraftServer
+
+
+def main():
+
+    # If you know the host and port, you may skip this and use MinecraftServer("example.org", 1234)
+    server = MinecraftServer("127.0.0.1:25565")
+
+    while 1:
+        # 'status' is supported by all Minecraft servers that are version 1.7 or higher.
+        status = server.status()
+        print("[{2}] The server has {0} players and replied in {1} ms".format(
+            status.players.online,
+            status.latency,
+            time.strftime("%d.%m.%Y %H:%M:%S")))
+        sleep(5)
+
+
+main()

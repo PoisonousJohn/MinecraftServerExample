8cb88cdf8ab61d0014285cdf0773a41cfca9d0b4
Ivan Fateev <ivan.fateev@microsoft.com>
2018-01-22 17:02:56 +0300

    fix shell to bash, becase sourcing

--- launch.sh
+++ launch.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 sudo apt-get update && sudo apt-get -y install python-pip
 python -m pip install mcstatus
 java -Xmx512M -Xms512M -jar minecraft_server.1.12.2.jar nogui&
+ls /etc
+echo "sourcing /etc/profile"
 source /etc/profile
 /usr/bin/python reportGameRooms.py& 2>> report.log
 wait

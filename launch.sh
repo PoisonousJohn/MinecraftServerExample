0e055e32bb3dff0768fcc8e027b6b898da70cc79
Ivan Fateev <ivan.fateev@microsoft.com>
2018-01-22 13:30:49 +0300

    added waiting of server to launch via exception handling

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get update && sudo apt-get -y install python-pip
 python -m pip install mcstatus
-/usr/bin/python reportGameRooms.py& >> report.log
 java -Xmx512M -Xms512M -jar minecraft_server.1.12.2.jar nogui&
+/usr/bin/python reportGameRooms.py& >> report.log
 wait
 echo "Server shutting down..."
--- reportGameRooms.py
+++ reportGameRooms.py
@@ -6,17 +6,23 @@ from mcstatus import MinecraftServer
 
 def main():
 
-    # If you know the host and port, you may skip this and use MinecraftServer("example.org", 1234)
-    server = MinecraftServer("127.0.0.1:25565")
-
+    # retry until started
     while 1:
-        # 'status' is supported by all Minecraft servers that are version 1.7 or higher.
-        status = server.status()
-        print("[{2}] The server has {0} players and replied in {1} ms".format(
-            status.players.online,
-            status.latency,
-            time.strftime("%d.%m.%Y %H:%M:%S")))
-        sleep(5)
+        try:
+            # If you know the host and port, you may skip this and use MinecraftServer("example.org", 1234)
+            server = MinecraftServer("127.0.0.1", 25565)
+
+            while 1:
+                # 'status' is supported by all Minecraft servers that are version 1.7 or higher.
+                status = server.status()
+                print("[{2}] The server has {0} players and replied in {1} ms".format(
+                    status.players.online,
+                    status.latency,
+                    time.strftime("%d.%m.%Y %H:%M:%S")))
+                sleep(5)
+        except:
+            print("Failed to connect to server. Retrying")
+            sleep(1)
 
 
 main()

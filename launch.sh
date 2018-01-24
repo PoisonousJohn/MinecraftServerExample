ee7ca408631d243a8314113e43537c1e2bf89c04
Ivan Fateev <ivan.fateev@microsoft.com>
2018-01-24 15:13:42 +0300

    added git pull to ensure the latest version of the repo added redirecting of output into log file

--- launch.sh
+++ launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get -y install python-pip
+set -x
+exec >> launch.log 2>&1
+git pull && sudo apt-get update && sudo apt-get -y install python-pip
 python -m pip install mcstatus
 java -Xmx512M -Xms512M -jar minecraft_server.1.12.2.jar nogui&
 ls /etc
@@ -7,4 +9,4 @@ echo "sourcing /etc/profile"
 source /etc/profile
 /usr/bin/python reportGameRooms.py& 2>> report.log
 wait
-echo "Server shutting down..."
+echo "Server is shutting down..."
